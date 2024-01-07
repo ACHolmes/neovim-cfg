54b1f80a39b9e0389d031a9d402f825e806d7f59
Andrew Holmes <acholmes001@gmail.com>
2024-01-07 22:36:47 +0000

    Adding some LSPs that I need, changed neo-tree shortcut to toggle

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+      ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "gopls",
+        -- "html", "tsserver" 
+        },
       auto_install = true,
     },
   },
@@ -21,15 +23,38 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
+
+      -- Set up Lua language server
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+
+      -- Setup rust analyzer / language server 
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities
+      })
+
+      -- Setup clangd -> C/C++ language server
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+
+      -- Setup for go language server 
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+
+      -- These LSPs require node.js, which I currently don't have installed on linux.
+      -- If needed, install node and uncomment here and above.
+      -- Setup tsserver for JavaScript langauge server
+      -- lspconfig.tsserver.setup({
+      --   capabilities = capabilities
+      -- })
+
+      -- Setup HTML language server
+      -- lspconfig.html.setup({
+      --   capabilities = capabilities
+      -- })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
